7617bb4c8872bbb24a7870dab348a529d9e66356
John Layher <johnevanlayher@gmail.com>
2023-02-20 16:45:18 -0500

    Setting up file structure

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+    </nav>
+  )
+}
+
+export default Nav
