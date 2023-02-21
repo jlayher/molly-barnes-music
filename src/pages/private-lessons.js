471209b6fd9618c949f2c4af23b0a3a0fb6a74d9
John Layher <johnevanlayher@gmail.com>
2023-02-20 20:48:40 -0500

    Added Testimonial Data and style changes for readability

--- src/pages/private-lessons.js
+++ src/pages/private-lessons.js
@@ -1,16 +1,35 @@
-import React from 'react'
-import Layout from '@/layout/Layout'
+import React from "react";
+import Layout from "@/layout/Layout";
 
 const PrivateLessons = () => {
   return (
     <Layout>
-    <h1>
-      Private Lessons Page
-      </h1>
-      <p>Info on private lessons</p>
+      <h1>Private Lessons Page</h1>
+      <p>
+        I have taught private violin lessons for many years, and my students
+        have ranged in age from 3 to 60. My teaching style is traditional (i.e.,
+        non-Suzuki), although I do frequently use Suzuki books as sources of
+        repertoire.
+        <br />
+        <br />
+        A successful student-teacher relationship depends on
+        respect, trust, and both parties' determination to achieve specific
+        goals, whether technical or musical in nature. I focus heavily on the
+        fundamental issues of rhythm and intonation, which form the basis for
+        excellent musicianship. I expect students to practice daily and to be
+        ready for and open to new challenges.
+        <br/>
+        <br/>
+        Currently I teach private lessons
+        in piano and violin, in-home and in-studio, through Skylark Music School
+        in Chapel Hill.
+        <br/>
+        <br/>
+        For more information about Skylark, please visit
+        <a href="https://www.skylarkmusicschool.com">https://www.skylarkmusicschool.com</a>
+      </p>
     </Layout>
+  );
+};
 
-  )
-}
-
-export default PrivateLessons
+export default PrivateLessons;
