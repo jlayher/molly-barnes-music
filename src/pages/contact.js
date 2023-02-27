c9de1567df5beb1f84e4f3e314f1c83762c58575
John Layher <johnevanlayher@gmail.com>
2023-02-27 18:26:47 -0500

    Some Deployment issues.  Updated page styles to be similar to home

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import Layout from '@/layout/Layout'
+import React from "react";
+import Layout from "@/layout/Layout";
+
+import styles from "@/styles/contact.module.css";
 
 const Contact = () => {
   return (
     <Layout>
-      <h1>
-        Contact Page
-      </h1>
-      <p>This is my basic contact info</p>
+      <div className={styles.contact__title}>Contact</div>
 
-      <div>
-        <h3>Downloads</h3>
-        <a href="">CV Download</a>
-        <a href="">PDF For Article</a>
+      <div className={styles.contact__body_container}>
+        <div className={styles.contact__body_text}>
+          <p>This is my basic contact info</p>
+
+          <div>
+            <h3>Downloads</h3>
+            <a href="">CV Download</a>
+            <a href="">PDF For Article</a>
+          </div>
+        </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/pages/private-lessons.js
+++ src/pages/private-lessons.js
@@ -1,31 +1,41 @@
 import React from "react";
 import Layout from "@/layout/Layout";
 
+import styles from "@/styles/privatelessons.module.css";
+
 const PrivateLessons = () => {
   return (
     <Layout>
-      <h1>Private Lessons Page</h1>
-      <p>
-        I have taught private violin lessons for many years, and my students
-        have ranged in age from 3 to 60. My teaching style is traditional (i.e.,
-        non-Suzuki), although I do frequently use Suzuki books as sources of
-        repertoire.
-        <br />
-        <br />
-        A successful student-teacher relationship depends on
-        respect, trust, and both parties' determination to achieve specific
-        goals, whether technical or musical in nature. I focus heavily on the
-        fundamental issues of rhythm and intonation, which form the basis for
-        excellent musicianship. I expect students to practice daily and to be
-        ready for and open to new challenges.
-        <br/>
-        <br/>
-        Currently I teach private lessons
-        in piano and violin, in-home and in-studio, through Skylark Music School
-        in Chapel Hill.
-        For more information about Skylark, please visit 
-        <a href="https://www.skylarkmusicschool.com"> https://www.skylarkmusicschool.com</a>
-      </p>
+      <div className={styles.privatelessons__title}>Private Lessons</div>
+
+      {/* body container */}
+      <div className={styles.privatelessons__body_container}>
+        <div className={styles.privatelessons__body_text}>
+          <p>
+            I have taught private violin lessons for many years, and my students
+            have ranged in age from 3 to 60. My teaching style is traditional
+            (i.e., non-Suzuki), although I do frequently use Suzuki books as
+            sources of repertoire.
+            <br />
+            <br />
+            A successful student-teacher relationship depends on respect, trust,
+            and both parties' determination to achieve specific goals, whether
+            technical or musical in nature. I focus heavily on the fundamental
+            issues of rhythm and intonation, which form the basis for excellent
+            musicianship. I expect students to practice daily and to be ready
+            for and open to new challenges.
+            <br />
+            <br />
+            Currently I teach private lessons in piano and violin, in-home and
+            in-studio, through Skylark Music School in Chapel Hill. For more
+            information about Skylark, please visit
+            <a href="https://www.skylarkmusicschool.com">
+              {" "}
+              https://www.skylarkmusicschool.com
+            </a>
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
