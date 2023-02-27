af841702820209f46975ca6d198a0b81395da7e6
John Layher <johnevanlayher@gmail.com>
2023-02-26 22:06:01 -0500

    Made Changes to Academic Page, and Home Page bg-colors

--- src/pages/academic-teaching.js
+++ src/pages/academic-teaching.js
@@ -1,55 +1,73 @@
-import Layout from '@/layout/Layout'
-import React from 'react'
+import Layout from "@/layout/Layout";
+import React from "react";
+
+import styles from "@/styles/academic.module.css";
 
 const AcademicTeaching = () => {
   return (
     <Layout>
-      {/* create a container for the page */}
-      {/* create a container for Academic teaching and show on left */}
-      <h1>Academic Teaching</h1>
-      <p>
-        I consider teaching music a passion and a calling, and I love getting to
-        know my students as human beings with real lives, hopes, fears, and
-        ambitions. My goal as a teacher--whether I am teaching music history,
-        musicianship skills, or the violin--is to share my love of the subject
-        such that my students are inspired to learn more and discover the joy
-        that can come from a deep intellectual and emotional engagement with
-        music.
-        <br />
-        <br />
-        {/* Add: You can view my Acacdemic evaluations here */} 
-        Excerpts from previous student evaluations of my teaching in
-        music history and musicianship classes at Durham University,
-        UNC-Greensboro, and UNC-Chapel Hill can be viewed by HERE.
-      </p>
+      {/* academic title container */}
+      <div className={styles.academic__title}>Academic Teaching & Research</div>
 
-      {/* remove breaks */}
-      <br/>
-      <br />
-      
-      {/* create a container for Research and show to the right */}
-      <h1>Research</h1>
-      <p>
-        My research primarily concerns issues of musical taste and the formation
-        of cultural hierarchies in the West during the nineteenth century. My
-        current book project (based on my PhD dissertation), tentatively titled
-        Old World Harmony, New World Democracy: American Idealism and the
-        European Musical Inheritance, 1830-1870, explores mid-nineteenth-century
-        American perceptions of the relationship between Western art music and
-        the ideal of democratic egalitarianism. Other research interests include
-        the musical culture of German immigrants to the United States in the
-        nineteenth century; music and social theory in the twentieth century,
-        particularly the thought of Adorno and the Frankfurt School; 1960s
-        psychedelic music in the U.S. and the U.K.; chamber music as a social
-        phenomenon in the nineteenth century; and many other topics.
-        <br />
-        <br />
-        My 2021
-        article in Nineteenth-Century Music Review and my 2016 dissertation are
-        available for download below.
-      </p>
+      {/* Content Container for Cards */}
+      <div className={styles.academic__body_container}>
+        <div className={styles.academic__body_text_container}>
+          
+
+          {/* create a container for Academic teaching and show on left */}
+          <div className={styles.academic__body_text_teaching}>
+            <h1>Academic Teaching</h1>
+            <p>
+              I consider teaching music a passion and a calling, and I love
+              getting to know my students as human beings with real lives, hopes,
+              fears, and ambitions. My goal as a teacher--whether I am teaching
+              music history, musicianship skills, or the violin--is to share my
+              love of the subject such that my students are inspired to learn more
+              and discover the joy that can come from a deep intellectual and
+              emotional engagement with music.
+              <br />
+              <br />
+              {/* Add: You can view my Acacdemic evaluations here */}
+              Excerpts from previous student evaluations of my teaching in music
+              history and musicianship classes at Durham University,
+              UNC-Greensboro, and UNC-Chapel Hill can be viewed in the
+              Testimonials tab above.
+            </p>
+          </div>
+
+          {/* remove breaks */}
+          <br />
+          <br />
+
+          {/* create a container for Research and show to the right */}
+          <div className={styles.academic__body_text_research}>
+            <h1>Research</h1>
+            <p>
+              My research primarily concerns issues of musical taste and the
+              formation of cultural hierarchies in the West during the nineteenth
+              century. My current book project (based on my PhD dissertation),
+              tentatively titled Old World Harmony, New World Democracy: American
+              Idealism and the European Musical Inheritance, 1830-1870, explores
+              mid-nineteenth-century American perceptions of the relationship
+              between Western art music and the ideal of democratic
+              egalitarianism. Other research interests include the musical culture
+              of German immigrants to the United States in the nineteenth century;
+              music and social theory in the twentieth century, particularly the
+              thought of Adorno and the Frankfurt School; 1960s psychedelic music
+              in the U.S. and the U.K.; chamber music as a social phenomenon in
+              the nineteenth century; and many other topics.
+              <br />
+              <br />
+              My 2021 article in Nineteenth-Century Music Review and my 2016
+              dissertation are available for download below.
+            </p>
+          </div>
+
+
+        </div>
+      </div>
     </Layout>
   );
-}
+};
 
-export default AcademicTeaching
+export default AcademicTeaching;
