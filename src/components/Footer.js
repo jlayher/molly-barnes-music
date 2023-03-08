b230bea0c3e3bb0454b307d689315a8ca74e4d3a
John Layher <johnevanlayher@gmail.com>
2023-03-08 16:34:42 -0500

    Fixed Footer Copyright styles.  Imported CV

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,43 +9,63 @@ import styles from '@/styles/footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer__container}>
-      {/* this contains the contact me directly links */}
-      <div className={[styles.footer__section_container, styles.footer__section_container_private]}>
-        <h3>Contact Me Directly</h3>
-        <span>Email: </span>
-          <a href="mailto: molly.l.barnes@gmail.com" className={styles.footer__section_container_anchors}>Molly.L.Barnes@gmail.com</a>
+      
+      <div className={styles.footer__container_toplinks}>
+         {/* this contains the contact me directly links */}
+        <div className={[styles.footer__section_container, styles.footer__section_container_private]}>
+          <h3>Contact Me Directly</h3>
+          <span>Email: </span>
+            <a href="mailto: molly.l.barnes@gmail.com" className={styles.footer__section_container_anchors}>Molly.L.Barnes@gmail.com</a>
+        </div>
+
+        {/* this contains the Private Lesson/Company Links */}
+        <div className={[styles.footer__section_container, styles.footer__section_container_private]}>
+          <h3>Private Lessons from Skylark Music School</h3>
+          <span>Contact Number: </span>
+          <a href="tel: 9192059849" className={styles.footer__section_container_anchors}>(919) 205-9849</a>
+            <br />
+          <span>Website: </span>
+            <a href="https://www.skylarkmusicschool.com/" className={styles.footer__section_container_anchors}> Skylark Music School</a>
+            <br />
+          <span>Email: </span>
+            <a href="mailto: info@skylarkmusicschool.com" className={styles.footer__section_container_anchors}>info@skylarkmusicschool.com</a>
+        </div>
+
+        {/* this contains the Dowload Links */}
+        <div className={[styles.footer__section_container, styles.footer__section_container_private]}>
+          <h3>Downloads</h3>
+          <a href="barnes_the_other_otto_dresel_ncmr.pdf" className={styles.footer__section_container_anchors} download="barnes_the_other_otto_dresel_ncmr.pdf">CV</a>
+          
+          <br/>
+          <a href="barnes_the_other_otto_dresel_ncmr.pdf" className={styles.footer__section_container_anchors} download="barnes_the_other_otto_dresel_ncmr.pdf">Article Download</a>
+        </div>
+
       </div>
 
-      {/* this contains the Private Lesson/Company Links */}
-      <div className={[styles.footer__section_container, styles.footer__section_container_private]}>
-        <h3>Private Lessons from Skylark Music School</h3>
-        <span>Contact Number: </span>
-        <a href="tel: 9192059849" className={styles.footer__section_container_anchors}>(919) 205-9849</a>
-          <br />
-        <span>Website: </span>
-          <a href="https://www.skylarkmusicschool.com/" className={styles.footer__section_container_anchors}> Skylark Music School</a>
-          <br />
-        <span>Email: </span>
-          <a href="mailto: info@skylarkmusicschool.com" className={styles.footer__section_container_anchors}>info@skylarkmusicschool.com</a>
-      </div>
+      
 
-      {/* this contains the Dowload Links */}
-      <div className={[styles.footer__section_container, styles.footer__section_container_private]}>
-        <h3>Downloads</h3>
-        <a href="barnes_the_other_otto_dresel_ncmr.pdf" className={styles.footer__section_container_anchors} download="barnes_the_other_otto_dresel_ncmr.pdf">CV</a>
-        
-        <br/>
-        <a href="barnes_the_other_otto_dresel_ncmr.pdf" className={styles.footer__section_container_anchors} download="barnes_the_other_otto_dresel_ncmr.pdf">Article Download</a>
-      </div>
+
+
+
+
+{/* footer bottom for copyright
+      goal is to make copyright and links at bottom left */}
+
 
       {/* this contains the Copyright Info */}
-      {/* Add my personal information, copyright, and any references to assets used */}
-      {/* <div className={styles.footer__section_container}>
-        <h3>Private Lessons</h3>
-        <p>Resume</p>
-        <p>CV</p>
-        <p>Article on blah blah bloo</p>
-      </div> */}
+      <div className={styles.footer__container_bottom}>
+        <div className={styles.footer__container_bottom_copyright}>&copy;2023 John Layher: &nbsp;Web Developer. &nbsp; All Rights Reserved </div>
+ 
+        <div className={styles.footer__container_bottom_links}>
+          <span className={styles.footer__container_bottom_links}>Email: &nbsp;</span>
+          <a href="mailto: john.e.layher@gmail.com" className={[ styles.footer__section_container_anchors, styles.footer__container_bottom_links]}>john.e.layher@gmail.com </a>
+  
+          {/* <span>Portfolio: </span>
+          <a href="https://www.johnlayher-portfolio.com/" className={styles.footer__section_container_anchors}> https://www.johnlayher-portfolio.com/</a> */}
+        </div>
+        
+      </div>
+
     </div>
   );
 }
