8ab1d1902439a843f99b3ee7ab0ce25668bfa2ad
John Layher <johnevanlayher@gmail.com>
2023-04-01 22:04:54 -0400

    changes to constants, style to academic, small margin changes to all

--- src/constants/besttestimonials.js
+++ src/constants/besttestimonials.js
@@ -2,46 +2,46 @@ export const bestTestimonials = [
   {
     id: "d6",
     text: "Molly Barnes was incredibly passionate about everything she taught. Always available to discuss work and went above and beyond to help me with any questions I had. Truly a fantastic lecturer.",
-    course: "(Historical Studies 2)",
+    course: "(Historical Studies 2, Durham University)",
   },
   {
     id: "d8",
     text: "Brilliant lectures, Molly explained everything very clearly but also challenged us to think and reason beyond the lectures in a friendly and encouraging way.",
-    course: "(Historical Studies 2)",
+    course: "(Historical Studies 2, Durham University)",
   },
   {
     id: "d9",
     text: "I really appreciated the dedication Dr. Barnes had to giving us individual tutorials to discuss summative topics and formative feedback, as I know very few other module leaders would offer up an opportunity like that.",
-    course: "(Historical Studies 2)",
+    course: "(Historical Studies 2, Durham University)",
   },
   {
     id: "d13",
     text: "Molly Barnes is an outstanding lecturer: informative, passionate, and keen to help her students in times of stress.",
-    course: "(Historical Studies 2)",
+    course: "(Historical Studies 2, Durham University)",
   },
   {
     id: "g1",
     text: "I absolutely loved this class and Dr. Barnes. She always conveyed the information in a way that was easy to understand while also pushing us to recall and make connections as we progressed throughout the semester.",
-    course: "(History of Western Music 1750-Present)",
+    course: "(History of Western Music 1750-Present, UNC-Greensboro)",
   },
   {
     id: "g3",
     text: "I didn't realize I loved music history so much! Dr. Barnes' passion and great teaching made me appreciate and enjoy music history more than I thought I would.",
-    course: "(History of Western Music 1750-Present)",
+    course: "(History of Western Music 1750-Present, UNC-Greensboro)",
   },
   {
     id: "g7",
     text: "Dr. Barnes is very welcoming and wants all students to be successful. It is obvious that she wants all of her students to succeed. She encourages students to ask her questions and she does whatever she can to ensure that we feel confident.",
-    course: "(History of Western Music 1750-Present)",
+    course: "(History of Western Music 1750-Present, UNC-Greensboro)",
   },
   {
     id: "g8",
     text: "Dr. Barnes is obviously very passionate and informed about what she is teaching, and it makes it so much more interesting to be a part of the class.",
-    course: "(History of Western Music 1750-Present)",
+    course: "(History of Western Music 1750-Present, UNC-Greensboro)",
   },
   {
     id: "g15",
     text: "I think Dr. Barnes is incredibly valuable to the music history program and UNCG as a whole!",
-    course: "(History of Western Music 1750-Present)",
+    course: "(History of Western Music 1750-Present, UNC-Greensboro)",
   },
 ];
--- src/pages/academic-teaching.js
+++ src/pages/academic-teaching.js
@@ -1,53 +1,50 @@
 import Layout from "@/layout/Layout";
 import React from "react";
-
+import Image from "next/image";
 import styles from "@/styles/academic.module.css";
 import SidepanelTest from "@/components/Body-Components/SidepanelTest";
-
+import mollyImg from "../../public/molly_black_full.jpg";
 const AcademicTeaching = () => {
   return (
     <Layout>
-      {/* academic title container */}
-   
-
-      {/* Body Content Container */}
       <div className={styles.academic__body_container}>
-        
-        {/* Container for Academic Teaching and Research Cards */}
+   
         <div className={styles.academic__body_text_container}>
           
-          {/* create a container for Academic teaching and show on left */}
-          <div className={styles.academic__body_text_teaching}>
-     <div className={styles.academic__title}>Academic Teaching</div>
-            <p>
-              I consider teaching music a passion and a calling, and I love
-              getting to know my students as human beings with real lives,
-              
-              hopes, fears, and ambitions. Whether I am
-              teaching music history, musicianship skills, or the violin, my goal is to
-              share my love of the subject such that my students are inspired to
-              learn more and discover the joy that can come from a deep
-              intellectual and emotional engagement with music.
-              <br />
-              <br />
-              Excerpts from previous student evaluations of my teaching in music
-              history and musicianship classes at Durham University,
-              UNC-Greensboro, and UNC-Chapel Hill can be viewed in the
-              Teaching Evaluations tab above.
-            </p>
+          <div className={styles.academic__photo_quote_container}>
+            
+            <div className={styles.academic__body_img_container}>
+              <Image src={mollyImg} alt="profile image" />
+            </div>
+
+            <div className={styles.academic__body_sidepanel_container}>
+              <SidepanelTest></SidepanelTest>
+            </div>
           </div>
-
-          {/* flex right */}
-          {/* <div className={styles.academic__body_sidepanel_container}>
-            <SidepanelTest></SidepanelTest>
-          </div> */}
-
- 
+    
+          <div className={styles.academic__body_text_teaching}>
+            
+              <div className={styles.academic__title}>Academic Teaching</div>
+              <div className={styles.academic__body_textsection}>
+                <p>
+                  I consider teaching music a passion and a calling, and I love
+                  getting to know my students as human beings with real lives,
+                  hopes, fears, and ambitions. Whether I am teaching music
+                  history, musicianship skills, or the violin, my goal is to
+                  share my love of the subject such that my students are
+                  inspired to learn more and discover the joy that can come from
+                  a deep intellectual and emotional engagement with music.
+                </p>
+                <p>
+                  Excerpts from previous student evaluations of my teaching in
+                  music history and musicianship classes at Durham University,
+                  UNC-Greensboro, and UNC-Chapel Hill can be viewed in the
+                  Teaching Evaluations tab above.
+                </p>
+              </div>
+            
+            </div>
         </div>
-
-        {/* Side Panel for Testimonials */}
-
-        
       </div>
     </Layout>
   );
